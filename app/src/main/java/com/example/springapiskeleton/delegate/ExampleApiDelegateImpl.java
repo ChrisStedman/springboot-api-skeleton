fd20ae78a3a309bb6cf602137d7873900f3297e7
ChrisStedman <christopher.john.stedman@gmail.com>
2023-02-17 10:41:22 +1100

    Implement service call from delegate

--- app/src/main/java/com/example/springapiskeleton/delegate/ExampleApiDelegateImpl.java
+++ app/src/main/java/com/example/springapiskeleton/delegate/ExampleApiDelegateImpl.java
@@ -2,17 +2,23 @@ package com.example.springapiskeleton.delegate;
 
 
 import com.example.springapiskeleton.api.ExampleApiDelegate;
-import com.example.springapiskeleton.api.model.GetExample200Response;
+import com.example.springapiskeleton.api.model.Example;
+import com.example.springapiskeleton.service.ExampleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ExampleApiDelegateImpl implements ExampleApiDelegate {
 
+    private final ExampleService exampleService;
+
     @Override
-    public ResponseEntity<GetExample200Response> getExample() {
-        GetExample200Response response = new GetExample200Response();
-        response.status("Success");
+    public ResponseEntity<List<Example>> getExample() {
+        List<Example> response = exampleService.getExamples();
 
         return ResponseEntity.ok().body(response);
     }
