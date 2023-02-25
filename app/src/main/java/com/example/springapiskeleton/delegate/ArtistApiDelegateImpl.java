73bcdf4073bbeef73ae69916a4a6b6c6ae26c1e5
ChrisStedman <christopher.john.stedman@gmail.com>
2023-02-25 21:53:57 +1100

    Implement use of services in ApiDelegate

--- app/src/main/java/com/example/springapiskeleton/delegate/ArtistApiDelegateImpl.java
+++ app/src/main/java/com/example/springapiskeleton/delegate/ArtistApiDelegateImpl.java
@@ -3,6 +3,9 @@ package com.example.springapiskeleton.delegate;
 
 import com.example.springapiskeleton.api.ArtistApiDelegate;
 import com.example.springapiskeleton.api.model.Artist;
+import com.example.springapiskeleton.api.model.ArtistBase;
+import com.example.springapiskeleton.service.ArtistService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,17 +13,22 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class ArtistApiDelegateImpl implements ArtistApiDelegate {
+
+    private final ArtistService artistService;
 
     @Override
     public ResponseEntity<List<Artist>> getArtists() {
-        List<Artist> response = List.of(new Artist());
+        List<Artist> artists = artistService.getAllArtists();
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(artists);
     }
 
     @Override
-    public ResponseEntity<Artist> createArtist(Artist artist){
+    public ResponseEntity<Artist> createArtist(ArtistBase artistBase){
+        Artist artist = artistService.createArtist(artistBase);
+
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(artist);
