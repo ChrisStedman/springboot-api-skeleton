59d608a3528a51ffa8091d08bf55cb17dce8d2af
ChrisStedman <christopher.john.stedman@gmail.com>
2023-02-26 11:56:11 +1100

    Delete classes related to base example

--- app/src/main/java/com/example/springapiskeleton/dao/ExampleTableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springapiskeleton.dao;
-
-import com.example.springapiskeleton.models.Example;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExampleTableRepository extends JpaRepository<Example, Integer> {
-}
